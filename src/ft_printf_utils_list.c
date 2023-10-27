842e055d7f40f0b2fb5e16a734ea8c9b3f5430e5
Yamir Slin Ramirez Fernandez <yaramire@cbr14s5.42barcelona.com>
2023-10-27 17:29:42 +0200

    add functions

---
+++ src/ft_printf_utils_list.c
@@ -0,0 +1,43 @@
+#include "../includes/ft_printf.h"
+
+void	**set_functions(void)
+{
+	int	(**operaciones)(va_list, int *);
+
+	operaciones = (int (**)(va_list, int *)) \
+	malloc(10 * sizeof(int (*)(va_list, int *)));
+	if (operaciones == NULL)
+		return (NULL);
+	operaciones[0] = ft_putchar_list;
+	operaciones[1] = ft_putstr_list;
+	operaciones[2] = ft_putpointer_list;
+	operaciones[3] = ft_putnbr_list;
+	operaciones[4] = ft_putnbr_list;
+
+	return ((void **)operaciones);
+}
+
+int	ft_putchar_list(va_list apoint, int *cntchr)
+{
+	ft_putchar(va_arg(apoint, int), cntchr);
+	return (*cntchr);
+}
+
+int	ft_putstr_list(va_list apoint, int *cntchr)
+{
+	ft_putstr(va_arg(apoint, char *), cntchr);
+	return (*cntchr);
+}
+
+int	ft_putpointer_list(va_list apoint, int *cntchr)
+{
+	ft_putstr("0x", cntchr);
+	ft_nbase((t_ul)va_arg(apoint, void *), 16, cntchr, "0123456789abcdef");
+	return (*cntchr);
+}
+
+int	ft_putnbr_list(va_list apoint, int *cntchr)
+{
+	ft_putnbr(va_arg(apoint, int), cntchr);
+	return (*cntchr);
+}
